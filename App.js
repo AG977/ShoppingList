70ce1b4637cfa49871bfd5cfc7f27af9d5d22a89
AG977 <93376305+AG977@users.noreply.github.com>
2025-11-28 11:47:52 +0100

    Update App.js
    update

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 // import react Navigation
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-//import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
-//import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage'; 
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage'; 
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 
 
 
@@ -52,9 +52,9 @@ const App = () => {
   
   //const analytics = getAnalytics(app);
 
-  //const auth = initializeAuth(app, {
-    //persistence: getReactNativePersistence(ReactNativeAsyncStorage)
-    //});
+  const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+    });
 
   // Initialize Cloud Firestore and get a reference to the service
   const db = getFirestore(app);
